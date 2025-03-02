7eea3a491e53c232915e5f07b228f86f4a15111f
chuckles201 <charlieawesome.15@gmail.com>
2025-03-01 22:05:28 -0500

    autosocial-deployment Sat Mar  1 22:05:28 EST 2025

--- post.sh
+++ post.sh
@@ -13,12 +13,11 @@ git push origin
 
 # sending usernmae and password
 expect <<EOF
-spawn git push -f origin
-expect "Username for 'https://github.com':"
-send "$username\r" 
-expect "Password for 'https://$username@github.com':"
-send "$password\r"
-expect eof
+    expect "Username for 'https://github.com':"
+    send "$username\r" 
+    expect "Password for 'https://$username@github.com':"
+    send "$password\r"
+    expect eof
 EOF
 
 echo Succesfully Pushed!
